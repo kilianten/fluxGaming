1a8da433102d0c41fcf86c01f5655a3c4322692d
kiliantennyson@live.ie <x00104769@gmail.com>
2018-03-13 14:49:30 +0000

    added User.java file

---
+++ fluxGaming/app/models/users/User.java
@@ -0,0 +1,77 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import io.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity
+public class User extends Model {
+
+    @Id
+    private Long id;
+    private String username;
+    private String role;
+    private String email;
+    private String password;
+
+    public User(Long id, String username, String role, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static Finder<String, User> Find = new Finder<String, User>(User.class);
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static List<User> findAll(){
+        return User.find.all();
+    }
+
+    public static User authenticate(String username, String password){
+        return find.query().where().eq("username", username).eq("password", password).findUnique();
+    }
+}
--- fluxGaming/conf/evolutions/default/1.sql
+++ fluxGaming/conf/evolutions/default/1.sql
@@ -13,8 +13,19 @@ create table review (
   constraint pk_review primary key (id)
 );
 
+create table user (
+  id                            bigint auto_increment not null,
+  username                      varchar(255),
+  role                          varchar(255),
+  email                         varchar(255),
+  password                      varchar(255),
+  constraint pk_user primary key (id)
+);
+
 
 # --- !Downs
 
 drop table if exists review;
+
+drop table if exists user;
 
