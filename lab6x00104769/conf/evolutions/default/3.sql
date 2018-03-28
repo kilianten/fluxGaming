00e997da5621e4a5fb0031e789512a5be6124d82
kilianten <kiliantennyson@live.ie>
2018-03-28 20:41:13 +0100

    removed other labFile

--- lab6x00104769/conf/evolutions/default/3.sql
+++
@@ -1,8 +0,0 @@
-# --- !Ups
-delete from user;
-
-insert into user (email,name,password,role) values ( 'admin@products.com', 'Alice Admin', 'password', 'admin' );
-
-insert into user (email,name,password,role) values ( 'manager@products.com', 'Bob Manager', 'password', 'manager' );
-
-insert into user (email,name,password,role) values ( 'customer@products.com', 'Charlie Customer', 'password', 'customer' );
--- lab6x00104769/public/javascripts/main.js
+++
@@ -1,3 +0,0 @@
-function confirmDel() {
-    return confirm("Are you sure?")
-}
